694949510360b7658ffdfdc5540ea43a42b4ea00
Linkcube <linkcubecode@gmail.com>
2025-01-15 12:03:27 -0500

    Install shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Button>Click me</Button>
