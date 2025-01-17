e8423694598655b5ec1793347028c02532adb1b9
Linkcube <poohbah125@gmail.com>
2025-01-17 09:38:59 -0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,6 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
 			precompress: false,
 			strict: true
 		}),
