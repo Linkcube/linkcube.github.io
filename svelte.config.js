54706dc0345cad1a904fc7fd177ea73c51d157c7
Linkcube <poohbah125@gmail.com>
2025-01-17 09:47:44 -0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: '/'
 		},
 		alias: {
 			'@/*': './path/to/lib/*'
